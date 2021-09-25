45b0f31865fb5088872c188cc84e8de85a2e22e4
ayodiya <devayodiya@gmail.com>
2021-09-25 09:42:32 +0100

    add custom theme

---
+++ client/src/utils/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles'
+export const theme = createTheme({
+  palette: {
+    ButtonColor: {
+      success: '#1ECE95',
+      danger: '#D8437C'
+    },
+    backgroundColor: {
+      main: '#4310C8',
+      light: '#360EA4',
+      dark: '#2A0C7D'
+    }
+  }
+})
